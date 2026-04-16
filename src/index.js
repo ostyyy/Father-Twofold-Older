a07552d00376ec04fa1d5137a5d53a48d4cb3512
ostyyy <ggg468432@gmail.com>
2026-04-16 19:17:00 +0300

    Add getMessage function

--- src/index.js
+++ src/index.js
@@ -17,6 +17,18 @@ function getYearsToDoubleAge(fatherAge, sonAge) {
     return years;
 }
 
+function getMessage(years) {
+    if (years > 0) {
+        return `Батько буде удвічі старший за сина через ${years} ${years % 10 ? 'рік' : 'років'}`;
+    } else if (years < 0) {
+        const absYears = Math.abs(years);
+
+        return `Батько був удвічі старший за сина ${absYears} ${absYears % 10 ? 'рік' : 'років'} тому`;
+    } else {
+        return `Зараз батько удвічі старший за сина`;
+    }
+}
+
 const years = getYearsToDoubleAge(61, 20);
 
-
+console.log(getMessage(years));
