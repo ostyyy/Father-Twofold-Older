2c0fe4e051417cef1429ed70e773ffe2771c2a82
ostyyy <ggg468432@gmail.com>
2026-04-16 19:16:09 +0300

    Add getYearsToDoubleAge() function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/**
+ *
+ * @param {number} fatherAge
+ * @param {number} sonAge
+ * @returns {number}
+ */
+
+function getYearsToDoubleAge(fatherAge, sonAge) {
+    if (fatherAge < 0 || sonAge < 0 || fatherAge - sonAge < 15) {
+        return NaN;
+    }
+
+    const years = fatherAge - 2 * sonAge;
+
+    return years;
+}
+
+const years = getYearsToDoubleAge(61, 20);
+
+
