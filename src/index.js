fcf14ea3aac9f504360306db8ad909eb0891c2f4
ostyyy <ggg468432@gmail.com>
2026-04-16 19:34:15 +0300

    Add getYearWord  function

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@
  */
 
 function getYearsToDoubleAge(fatherAge, sonAge) {
-    if (fatherAge < 0 || sonAge < 0 || fatherAge - sonAge < 15) {
+    if (
+        fatherAge < 0 || 
+        sonAge < 0 || 
+        fatherAge - sonAge < 15
+    ) {
         return NaN;
     }
 
@@ -17,18 +21,38 @@ function getYearsToDoubleAge(fatherAge, sonAge) {
     return years;
 }
 
+function getYearWord (num) {
+    const last = num % 10;
+    const lastTwo = num % 100;
+
+    if(lastTwo >= 11 && lastTwo <= 14) { return 'років'; }
+
+    if(last === 1) { return 'рік'; }
+
+    if(last >= 2 && last <= 4) { return 'роки'; }
+
+    return 'років';
+}
+
 function getMessage(years) {
+
+    if (!years){
+        return 'НЕкоректні дані!'
+    }
+
     if (years > 0) {
-        return `Батько буде удвічі старший за сина через ${years} ${years % 10 ? 'рік' : 'років'}`;
+        return `Батько буде удвічі старший за сина через ${years} ${getYearWord(years)}`;
     } else if (years < 0) {
         const absYears = Math.abs(years);
 
-        return `Батько був удвічі старший за сина ${absYears} ${absYears % 10 ? 'рік' : 'років'} тому`;
+        return `Батько був удвічі старший за сина ${absYears} ${getYearWord(absYears)} тому`;
     } else {
         return `Зараз батько удвічі старший за сина`;
     }
 }
 
-const years = getYearsToDoubleAge(61, 20);
+
+
+const years = getYearsToDoubleAge(54, 10);
 
 console.log(getMessage(years));
